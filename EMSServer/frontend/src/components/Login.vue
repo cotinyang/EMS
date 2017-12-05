12c7083c152334e333d8e139021b4c0b7b38f736
cotin <yangketian@gmail.com>
2017-12-05 15:57:14 +0800

    add login view

---
+++ EMSServer/frontend/src/components/Login.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="login">
+    <div class="login-wrap" v-show="showLogin">
+      <div class = "login-title">登录</div>
+      <input class = "login-input login-item" type="text" placeholder="请输入账号">
+      <input class = "login-input login-item" type="text" placeholder="请输入密码">
+      <input class = "login-button login-item"type="button" value="确认">
+    </div>
+    <div class="login-wrap" v-show="!showLogin">
+      <div class = "login-title">注册</div>
+      <input class = "login-input login-item" type="text" placeholder="请输入账号">
+      <input class = "login-input login-item" type="text" placeholder="请输入密码">
+      <input class = "login-input login-item" type="text" placeholder="请重复密码">
+      <input class = "login-button login-item"type="button" value="确认">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data () {
+    return {
+      msg: '注册账号',
+      showLogin: true,
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+.login-wrap {
+  text-align: center;
+}
+
+.login-item {
+  display: block;
+  width: 250px; 
+  height: 40px; 
+  line-height: 40px; 
+  margin: auto;
+}
+
+.login-input {
+  margin-bottom: 10px; 
+  outline:none; 
+  border:1px solid #888; 
+  padding:10px; 
+  box-sizing:border-box;
+}
+
+.login-button {
+  border:none; 
+  background-color:#41b883; 
+  color:#fff; 
+  font-size:16px; 
+  margin-bottom:5px;
+}
+
+</style>
--- EMSServer/model/DBManager.js
+++ EMSServer/model/DBManager.js
@@ -5,7 +5,8 @@ var DBManager = module.exports = function DBManager(path) {
   if (!(this instanceof DBManager)) {
     return new DBManager(path);
   }
-  this.db = monk(path);
+  var db = monk(path);
+  this.userCollection = db.get('user');  
   var mid = function (req, res, next) {
     req.dbManager = this;
     next();
@@ -13,16 +14,33 @@ var DBManager = module.exports = function DBManager(path) {
   return mid.bind(this);
 };
 
+/**
+ * Shema user
+ * {
+ *  userName: String, unique,index
+ *  nickName: String,
+ *  password: String,
+ *  userEmail: String,  unique,index
+ *  phoneNumber: String, unique,index
+ * }
+ * 
+ * Shema contact
+ * Shema group
+ * 
+ */
+
 DBManager.prototype.userList = function userList(callback) {
-  var collection = this.db.get('usercollection');
-  collection.find({}, {}, callback);
+  this.userCollection.find({}, {}, callback);
 }
 
 
-// DBManager.prototype.addUser = function addUser(userName, userEmail, callback) {
-//   var collection = this.db.get('usercollection');
-//   collection.insert({
-//     "username": userName,
-//     "email": userEmail
-//   }, callback);
-// }
+DBManager.prototype.addUser = function addUser(userName, userEmail, callback) {
+  this.userCollection.insert({
+    "username": userName,
+    "email": userEmail
+  }, callback);
+}
+
+//authentication
+//register
+//logout
